d23a5ac152d2b0c5b3c4a08af665495f39f38a9b
Charles De-Wnd <cdewind@outlook.com>
2020-06-04 23:59:24 +0100

    wrote html code and views code to make model-objects be displayed in homepage

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def home(request):
+    projectsObj = Project.objects.all()
+    
+    return render(request, "portfolio/index.html", {"projects":projectsObj})
