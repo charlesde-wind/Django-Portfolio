b3941d5788220f45953cfa12b067fbbbf2dd0896
Charles De-Wnd <cdewind@outlook.com>
2020-06-05 18:37:59 +0100

    Created Blogs Model and Adding Static Files into the website

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    # blog name, blog description, blog date, blog author
+    blog_name = models.CharField(max_length=250)
+    blog_author= models.CharField(max_length=40, blank=True)
+    blog_date = models.DateField()
+    blog_description = models.CharField(max_length=500)
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def all_blogs(request):
+    blogobjects = Blog.objects.order_by("-blog_date")[:5]
+    
+    return render(request, "blog/all_blogs.html", {"blogobjects":blogobjects})
